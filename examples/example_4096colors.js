08f90a1d5a12b923e2ad2bfbb20c1f604e6d6ad4
DomLeMarin <christianraemy@gmail.com>
2021-09-29 19:48:14 +0200

    Making distinction between websafe-colors format and 4096-colors format. Adding a compression algo for the 4096-colors format

--- examples/example_4096colors.js
+++ examples/example_4096colors.js
@@ -1,5 +1,5 @@
-require('./pixelmapCompression.js')
-require('./generateExample.js')
+require('../src/pixelmapCompression_4096colors.js')
+require('./generateExample_4096colors.js')
 
 const sample1 = [
   '390390390390390390390000000390390390390390390390',
@@ -58,6 +58,25 @@ const sample3 = [
   '9fcf3399c03390300c03c693636c0cf9f0f6cf0cc0c933c3'
 ]
 
+const sample4 = [
+  'fa43c98ab4bc5a8866b0142a0ff8f086aff5542a112f43d1',
+  'e4eb2677d72b11a6cd47fe948d81eac20d6582cd127129af',
+  'f7b5a6673a20b1256a98a9e86f8368d1b02fcc55970c0a2d',
+  'f67222b7457dfafcbacae68e66ba1e30e748d6b3e34418a8',
+  '593e208210b0511e59301676a0de6aaac0e7b6b3a7a39935',
+  'ca0bbab8d1423770de8263589138f67338e85157850bb819',
+  'c1c77f4e022b14f410b8c5e4dff925669a358b70b93f7991',
+  '0d974e8840e4ad2c6eecbd02804ad15cd6e71f8c044efc9d',
+  'aea5fd9dc52f90927b7f12fd1b4d6c6c96c6c2496b6dcddb',
+  'ed6a9ae3a56e745a1813df407a398d98e260916d5f65c396',
+  '92ba622967128179a2b01947d7b2d1d9a2790875574a87f6',
+  'b845ac6bc9438b270e2155a4d9149871c2b913a11f451405',
+  '8aef3555198c0f2fe627f59638b8697f7eaf76b43f754a1a',
+  '4f67fd9ebcaf6960be531a929d345f2060112af9b17803e1',
+  'fdf088e954117785c4da63678a85906882709fe71fff19bc',
+  '0b3b145da2a6bf3a7aa223f4c885832a3d5c5ce7e13aea68'
+]
+
 let ss, encoded, decoded
 
 console.log("*************** SAMPLE 1 ***************")
@@ -93,11 +112,27 @@ console.log('Original Length:' + ss.length)
 console.log('Encoded Length:' + encoded.length)
 console.log('Ratio:' + ((1 - encoded.length/ss.length) * 100).toFixed(2) + '%')
 
-
+console.log("")
 console.log("*************** SAMPLE 3 ***************")
 
 //ss = generateRandomExample()
 ss = sample3.join("")
+
+encoded = encode(ss)
+decoded = decode(encoded)
+
+console.log('Original:' + ss)
+console.log('Encoded:' + encoded)
+console.log('Decoded: ' + decoded)
+console.log('Matching: ' + (ss == decoded))
+console.log('Original Length:' + ss.length)
+console.log('Encoded Length:' + encoded.length)
+console.log('Ratio:' + ((1 - encoded.length/ss.length) * 100).toFixed(2) + '%')
+
+console.log("")
+console.log("*************** SAMPLE 4 ***************")
+
+ss = sample4.join("")
 
 encoded = encode(ss)
 decoded = decode(encoded)
